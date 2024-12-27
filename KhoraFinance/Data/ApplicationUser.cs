741a77aae7cc2fb5f55774d95cba906a4a19234f
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-12-26 21:33:35 -0300

    upload inicial

---
+++ KhoraFinance/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace KhoraFinance.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
